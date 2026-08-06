c0e954b457dae745966d24d241cc717cb1023f96
Ghanshyam Sharma <ghanshyam18n@gmail.com>
2026-08-06 18:19:44 +0530

    Array123

---
+++ src/main/java/array/Array123.java
@@ -0,0 +1,14 @@
+package array;
+
+public class Array123 {
+	public static void main(String[] args) {
+		
+		String [] name = {"Shyam", "JDK", "Selenium", "API"};
+		
+		for(String s : name) {
+			
+			System.out.println(s);
+		}
+	}
+
+}
