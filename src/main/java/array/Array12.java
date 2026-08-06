cf780d646e816b71b21095f1781768ed689b47f1
Ghanshyam Sharma <ghanshyam18n@gmail.com>
2026-08-06 18:00:09 +0530

    Array12

---
+++ src/main/java/array/Array12.java
@@ -0,0 +1,14 @@
+package array;
+
+public class Array12 {
+
+	public static void main(String[] args) {
+		
+		int [] marks = {70,40, 56, 87, 98};
+		
+		for(int i = 0; i<marks.length; i++) {
+			
+			System.out.println(marks[i]);
+		}
+	}
+}
