70e5a7619dbce4dc4772619b626b982d53e0e5a2
Ghanshyam Sharma <ghanshyam18n@gmail.com>
2026-08-06 17:28:30 +0530

    DataTypes 1

---
+++ src/main/java/datatypes/PrimitiveDataTypes.java
@@ -0,0 +1,19 @@
+package datatypes;
+
+public class PrimitiveDataTypes {
+public static void main(String args[]) {
+		
+		int age = 22;
+		String name = "Shyam";
+		char grade = 'A';
+		double sal = 350000;
+		boolean selected = true;
+		
+		System.out.println("Name : " + name + 
+				"\nAge : " + age + 
+				"\nGrade : " + grade + 
+				"\nSelected : "+selected);
+		
+	}
+
+}
