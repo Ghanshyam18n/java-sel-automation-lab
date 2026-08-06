686404ea6201996ac7c112c6725e4e8b682d9f22
Ghanshyam18n <ghanshyam18n@gmail.com>
2026-08-06 23:37:04 +0530

    added new prackage Practice_problem and even number

---
+++ src/main/java/practice_problem/EvenNum.java
@@ -0,0 +1,18 @@
+package practice_problem;
+
+public class EvenNum {
+             public static void main(String[] args) {
+				int [] number = {1,2,3,4,5,6,7,8,5,3,4,6,7,5,4,71,18,20};
+				
+				for(int even : number) {
+					
+					if(even % 2 == 0) {
+						System.out.println(even);
+					}
+				}
+				
+				// first 20 even number 
+				
+				
+			}
+}
