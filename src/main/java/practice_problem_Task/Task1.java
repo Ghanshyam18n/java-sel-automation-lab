9a4b47a854228f46baf2fa4d5ee9478450475824
Ghanshyam18n <ghanshyam18n@gmail.com>
2026-08-08 14:35:19 +0530

    Task1 added

---
+++ src/main/java/practice_problem_Task/Task1.java
@@ -0,0 +1,22 @@
+package practice_problem_Task;
+
+/*
+Task 1
+Create an array of browser names.
+Chrome
+Firefox
+Edge
+------- Print all browser
+*/
+public class Task1 {
+
+	public static void main(String[] args) {
+		
+		String[] browser = {"Chrome", "Firefox", "Edge"};
+		
+		for(String name : browser) {
+			
+			System.out.println(name);
+		}
+	}
+}
