61133b52a5bfe2c3691785162a0f379dd5622c3e
Ghanshyam18n <ghanshyam18n@gmail.com>
2026-08-08 14:28:59 +0530

    modified in even number prog

--- src/main/java/practice_problem/EvenNum.java
+++ src/main/java/practice_problem/EvenNum.java
@@ -11,8 +11,27 @@ public class EvenNum {
 					}
 				}
 				
-				// first 20 even number 
+				// print all even number from an array and print 
+				System.out.println("\nFind even number");
 				
+				int [] arr = {3,4,2,3,4,5,6,7};
+				
+				for(int i=0; i<arr.length; i++) {
+					
+					if(arr[i]%2==0) {
+						System.out.println(arr[i]);
+					}
+				}
+				
+				System.out.println("\nFind first even from array");
+				for(int i=0; i<arr.length; i++) {
+					
+					if(arr[i]%2==0) {
+						System.out.println(arr[i]);
+						break;
+					}
+				}
 				
 			}
 }
+ 
