7289a4d453fc3405c29ba9c84013ec1dbb72d0e9
Ghanshyam18n <ghanshyam18n@gmail.com>
2026-08-08 15:03:16 +0530

    task3 added

---
+++ src/main/java/practice_problem_Task/Task3.java
@@ -0,0 +1,31 @@
+package practice_problem_Task;
+
+/*
+Store test case IDs.
+Print them using both:
+normal for
+enhanced for */
+
+public class Task3 {
+	public static void main(String[] args) {
+		
+		String[] ID = {
+				  
+				"TS01", "TS02", "TS03"
+		};
+		
+		System.out.println("\nNormal for loop");
+		
+		for(int i=0; i<ID.length; i++) {
+			
+			System.out.println(ID[i]);
+		}
+		
+		System.out.println("\nEnhanced for loop");
+		
+		for(String test : ID) {
+			System.out.println(test);
+		}
+	}
+
+}
